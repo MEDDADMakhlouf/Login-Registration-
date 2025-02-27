8e6b24a2e5188ccbc123980b7282276ee655c8df
MEDDADMakhlouf <makhloufmeddad@gmail.com>
2025-02-27 18:48:39 -0500

    create app,screen, component package

---
+++ app/src/main/java/com/example/login_registration/components/AppComponents.kt
@@ -0,0 +1,28 @@
+package com.example.login_registration.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NormalTextComponent(value: String){
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxSize()
+            .heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
+
+        )
+    )
+}
---
+++ app/src/main/java/com/example/login_registration/screens/SignUpScreen.kt
@@ -0,0 +1,8 @@
+package com.example.login_registration.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignUpScreen (){
+
+}
