5b600e098cdf49bd14cc45869fee1447bd158c68
MEDDADMakhlouf <makhloufmeddad@gmail.com>
2025-02-27 22:30:30 -0500

    add few function to setup the style of text

--- app/src/main/java/com/example/login_registration/components/AppComponents.kt
+++ app/src/main/java/com/example/login_registration/components/AppComponents.kt
@@ -1,28 +1,53 @@
 package com.example.login_registration.components
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.login_registration.R
 
 @Composable
 fun NormalTextComponent(value: String){
     Text(
         text = value,
         modifier = Modifier
-            .fillMaxSize()
-            .heightIn(min = 80.dp),
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
-            fontStyle = FontStyle.Normal
+            fontStyle = FontStyle.Normal,
 
-        )
+        ),color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
     )
 }
+
+@Composable
+fun HeadingTextComponent(value: String){
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal,
+
+        ), color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+
+    )
+}
+
+@Composable
+fun MyTextField(){}
--- app/src/main/java/com/example/login_registration/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/login_registration/screens/SignUpScreen.kt
@@ -1,8 +1,39 @@
 package com.example.login_registration.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.login_registration.R
+import com.example.login_registration.components.HeadingTextComponent
+import com.example.login_registration.components.NormalTextComponent
 
 @Composable
 fun SignUpScreen (){
+    Surface (
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
+    ){
+        Column (
+            modifier = Modifier.fillMaxSize()
+        ){   NormalTextComponent(value = stringResource(id = R.string.hello) )
+            HeadingTextComponent(value = stringResource(id = R.string.Create_accoount))
+        }
+       }
+}
 
+
+@Preview
+@Composable
+fun DefaultPreviewOfSingUpScreen(){
+    SignUpScreen()
 }
