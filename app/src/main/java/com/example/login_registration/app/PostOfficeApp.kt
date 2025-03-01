ee0aa10c4939f2da1b08789c7af85476038bb7af
MEDDADMakhlouf <makhloufmeddad@gmail.com>
2025-03-01 00:11:34 -0500

    fix the white screen when open the app with putting SignUpScreen inside PostOfficeApp fun (:

--- app/src/main/java/com/example/login_registration/app/PostOfficeApp.kt
+++ app/src/main/java/com/example/login_registration/app/PostOfficeApp.kt
@@ -5,14 +5,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.example.login_registration.screens.SignUpScreen
+import com.example.login_registration.ui.theme.LoginRegistrationTheme
 
 @Composable
-fun PostOfficeApp(){
-    Surface (
-        modifier = Modifier.fillMaxSize(),
-        color = Color.White
+fun PostOfficeApp() {
+    LoginRegistrationTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.White
 
-        ){
+        ) {
+            SignUpScreen()
 
+        }
     }
 }
